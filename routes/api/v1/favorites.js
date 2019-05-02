93aef193d82db01ac374a95f36d9ce34f3bbae5c
manojpanta <manojpanta95@gmail.com>
2019-05-02 17:26:53 -0600

    Added favorites controller which creates favorites for a user based on the api key passed

---
+++ routes/api/v1/favorites.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+var User = require('../../../models').User;
+var Favorite = require('../../../models').Favorite;
+var pry = require('pryjs');
+
+router.post("/", function(req, res, next) {
+  User.findOne({
+    where: {
+      "api_key": req.body.api_key
+    }
+  })
+  .then(user => {
+    if (user) {
+      return Favorite.create({
+        location: req.body.location,
+        user_id: user.id
+      })
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(400).send(JSON.stringify({"error": "Invalid Api Key"}));
+    }
+  })
+  .then((favorite) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(JSON.stringify({"message": favorite.location + " has been added to your favorites."}));
+  })
+  .catch((error)=> {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({ error });
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
