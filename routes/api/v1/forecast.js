8e3751fa29566a9d7bbc9ffd80a1ed90dd5bb98e
manojpanta <manojpanta95@gmail.com>
2019-05-02 14:34:57 -0600

    now have working forecast for a city

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -4,15 +4,26 @@ var User = require('../../../models').User;
 const fetch = require('node-fetch');
 var pry = require('pryjs');
 require('dotenv').config();
+
 router.get("/", function(req, res, next) {
-  var lat = '';
-  var lon = '';
+  var location = req.query.location
   fetch("https://maps.googleapis.com/maps/api/geocode/json?address=denver&key=" + process.env.GEOCODING_API)
   .then((response) => response.json())
-  .then((result)=> {
-    lat = result["results"][0]["geometry"]["location"]["lat"]
-    lon = result["results"][0]["geometry"]["location"]["lon"]
-  });
+  .then((result)=>  {
+    let lat = result["results"][0]["geometry"]["location"]["lat"]
+    let lon = result["results"][0]["geometry"]["location"]["lng"]
+    return fetch("https://api.darksky.net/forecast/" + process.env.DARK_SKY_API_KEY + "/" + lat + "," + lon)
+    .then((response) => response.json())
+    .then((result) => {
+      let forecast = {
+        "location": location,
+        "currently": result["currently"],
+        "hourly": result["hourly"],
+        "daily": result["daily"]
+      }
+      res.send((forecast))
+    })
+  })
 });
 
 module.exports = router;
