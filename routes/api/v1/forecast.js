b3a95968d92f05f43c8b79d50fb1e6fc8e450c0f
manojpanta <manojpanta95@gmail.com>
2019-05-05 16:17:13 -0600

    using forecast-service.js's methods to retrive forecast for a city and added logic to check api-key' s presence

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -4,44 +4,39 @@ var User = require('../../../models').User;
 const fetch = require('node-fetch');
 var pry = require('pryjs');
 require('dotenv').config();
-
+var getForecast = require('../../../models').getForecast;
 
 router.get("/", function(req, res, next) {
-  var location = req.query.location
-  getForecast(location, req, res, next);
-});
-
-function getForecast(location, req, res, next) {
-
-  var url = new URL("https://maps.googleapis.com/maps/api/geocode/json"),
-  params = {address: location, key: process.env.GEOCODING_API}
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-
-  fetch(url)
-  .then((response) => response.json())
-  .then((result)=>  {
-    let lat = result["results"][0]["geometry"]["location"]["lat"]
-    let lon = result["results"][0]["geometry"]["location"]["lng"]
-    return fetch("https://api.darksky.net/forecast/" + process.env.DARK_SKY_API_KEY + "/" + lat + "," + lon)
-    .then((response) => response.json())
-    .then((result) => {
-      let forecast = _forecastFormatter(result, location);
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(JSON.stringify(forecast));
+  if (req.body.api_key) {
+    User.findOne({
+      where: {
+        "api_key": req.body.api_key
+      }
     })
-  })
-}
-
-
-
-
-function _forecastFormatter(result, location) {
-  return {
-    "location": location,
-    "currently": result["currently"],
-    "hourly": result["hourly"],
-    "daily": result["daily"]
+    .then(user => {
+      if (user) {
+        var location = req.query.location
+        var forecast;
+        new Promise(function(resolve, reject) {
+          forecast = getForecast(location);
+          resolve(forecast)
+        })
+        .then((result)=> {
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).send(JSON.stringify({"forecast": result}));
+        })
+        .catch((error) => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(401).send(JSON.stringify(error));
+        })
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(401).send(JSON.stringify({"error": "Invalid Api Key || API key  must present"}));
+      }
+    })
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({"error": "Invalid Api Key || API key must present"}));
   }
-}
-
+});
 module.exports = router;
